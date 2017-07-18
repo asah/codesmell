9d0218f2419b5de0cc647abfe4653d8ffca6925b
Adam Sah <asah@midgard.net>
2017-07-18 15:20:14 +0200

    add javascript

--- get-commit.sh
+++ get-commit.sh
@@ -5,7 +5,10 @@ HASH=$1
 FILETYPE=$2
 if [ "x$FILETYPE" = "xpython" ]; then      ext='"*.py" "*.py"';
 elif [ "x$FILETYPE" = "xjava" ]; then      ext='"*.java"';
-elif [ "x$FILETYPE" = "xc_and_cpp" ]; then ext='"*.c" "*.cc" "*.cpp" "*.cxx" "*.C"';
+elif [ "x$FILETYPE" = "xc_and_cpp" ]; then ext='"*.c" "*.h" "*.cc" "*.cpp" "*.cxx" "*.C "*.H""';
+elif [ "x$FILETYPE" = "xjs_backend" ]; then   ext='"*.js" "*.ejs"';
+elif [ "x$FILETYPE" = "xjs_frontend" ]; then  ext='"*.js" "*.ejs"';
+elif [ "x$FILETYPE" = "xjs" ]; then           ext='"*.js" "*.ejs"';
 else echo "unknown extension type, try <c_and_cpp|java|python|...>"; exit 1;
 fi
 
